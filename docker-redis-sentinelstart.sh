661834c2bbceb8e65aae7209dc3ef823a68463fd
caiex.1.144 <caiex@1.144>
2016-04-22 18:29:57 +0800

    modify: -v to start

--- docker-redis-sentinelstart.sh
+++ docker-redis-sentinelstart.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-docker run --net=host -d --name redis-6479 redis-6479
-docker run --net=host -d --name redis-6579 redis-6579
+
+docker run --net=host -d -v /opt/6479:/data --name redis-6479 redis-6479
+docker run --net=host -d -v /opt/6579:/data --name redis-6579 redis-6579
+#docker run --net=host -d --name redis-6479 redis-6479
+#docker run --net=host -d --name redis-6579 redis-6579
+
 docker run --net=host -d --name redis-26479 redis-26479
